b02f576a87888e39e57b25b9c9d0de53ac52d30a
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-11-03 08:48:18 -0600

    ice cream

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ $( document ).ready(function() {
 $('#translucent-borders').on('click', function(){
   $('.tb').toggleClass('visible');
 });
+
+$('#multiple-borders').on('click', function(){
+  $('.mb').toggleClass('visible');
+});
 });
